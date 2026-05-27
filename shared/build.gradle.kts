6d39c367576826686d6612fd6e44917c2d45b760
Pedro Guillén <pedro.guillen55@gmail.com>
2026-05-27 11:17:20 +0200

    Added common koin dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -48,6 +48,9 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.kotlinx.datetime)
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
