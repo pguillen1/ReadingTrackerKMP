1a8a9994681c04053b37b6cad4a8959523aae086
Pedro Guillén <pedro.guillen55@gmail.com>
2026-06-01 19:23:54 +0200

    Changed import of colors from LibraryScreen

--- shared/src/commonMain/kotlin/com/pguillen/readingtracker/presentation/library/LibraryScreen.kt
+++ shared/src/commonMain/kotlin/com/pguillen/readingtracker/presentation/library/LibraryScreen.kt
@@ -47,6 +47,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.pguillen.readingtracker.domain.model.Book
 import com.pguillen.readingtracker.domain.model.ReadingStatus
+import com.pguillen.readingtracker.presentation.theme.ReadingTrackerColors
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
