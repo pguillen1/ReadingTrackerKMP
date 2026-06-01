b6d9b572738a9e1a14f5b179cbe1a54e60edd2eb
Pedro Guillén <pedro.guillen55@gmail.com>
2026-06-01 19:26:16 +0200

    Added StatsScreenViewModel to presentation module

--- shared/src/commonMain/kotlin/com/pguillen/readingtracker/di/PresentationModule.kt
+++ shared/src/commonMain/kotlin/com/pguillen/readingtracker/di/PresentationModule.kt
@@ -1,9 +1,11 @@
 package com.pguillen.readingtracker.di
 
 import com.pguillen.readingtracker.presentation.library.LibraryViewModel
+import com.pguillen.readingtracker.presentation.stats.StatsViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val presentationModule = module {
 	viewModel { LibraryViewModel(get()) }
+	viewModel { StatsViewModel(get()) }
 }
