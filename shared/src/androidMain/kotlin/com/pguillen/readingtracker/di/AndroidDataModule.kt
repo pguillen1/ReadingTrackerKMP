ed54eb91fc7edeedb8db07f8df7f6435d34fb871
Pedro Guillén <pedro.guillen55@gmail.com>
2026-06-05 10:19:48 +0200

    Added database and repositories to AndroidDataModule

--- shared/src/androidMain/kotlin/com/pguillen/readingtracker/di/AndroidDataModule.kt
+++ shared/src/androidMain/kotlin/com/pguillen/readingtracker/di/AndroidDataModule.kt
@@ -2,6 +2,14 @@ package com.pguillen.readingtracker.di
 
 import com.pguillen.readingtracker.data.datastore.DataStoreUserPreferencesRepository
 import com.pguillen.readingtracker.data.datastore.createUserPreferencesDataStore
+import com.pguillen.readingtracker.data.local.AndroidDatabaseDriverFactory
+import com.pguillen.readingtracker.data.local.repository.SqlDelightBookNoteRepository
+import com.pguillen.readingtracker.data.local.repository.SqlDelightBookRepository
+import com.pguillen.readingtracker.data.local.repository.SqlDelightReadingSessionRepository
+import com.pguillen.readingtracker.database.ReadingTrackerDatabase
+import com.pguillen.readingtracker.domain.repository.BookNoteRepository
+import com.pguillen.readingtracker.domain.repository.BookRepository
+import com.pguillen.readingtracker.domain.repository.ReadingSessionRepository
 import com.pguillen.readingtracker.domain.repository.UserPreferencesRepository
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
@@ -16,6 +24,36 @@ val androidDataModule = module {
 	single<UserPreferencesRepository> {
 		DataStoreUserPreferencesRepository(
 			dataStore = get()
+		)
+	}
+
+	single {
+		AndroidDatabaseDriverFactory(
+			context = androidContext()
+		).createDriver()
+	}
+
+	single {
+		ReadingTrackerDatabase(
+			driver = get()
+		)
+	}
+
+	single<BookRepository> {
+		SqlDelightBookRepository(
+			database = get()
+		)
+	}
+
+	single<ReadingSessionRepository> {
+		SqlDelightReadingSessionRepository(
+			database = get()
+		)
+	}
+
+	single<BookNoteRepository> {
+		SqlDelightBookNoteRepository(
+			database = get()
 		)
 	}
 }
