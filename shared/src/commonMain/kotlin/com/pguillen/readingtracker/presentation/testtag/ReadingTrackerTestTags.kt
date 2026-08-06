28dfb95e12f28f99912956e8989df40a4cc2dfc3
Pedro Guillen <pedro.guillen55@gmail.com>
2026-08-06 13:11:10 +0200

    Added date picker test tag.

--- shared/src/commonMain/kotlin/com/pguillen/readingtracker/presentation/testtag/ReadingTrackerTestTags.kt
+++ shared/src/commonMain/kotlin/com/pguillen/readingtracker/presentation/testtag/ReadingTrackerTestTags.kt
@@ -47,6 +47,7 @@ object ReadingTrackerTestTags {
 		const val SCREEN_TITLE = "add_edit_session_screen_title"
 		const val BACK_BUTTON = "add_edit_session_back_button"
 		const val DATE_FIELD = "add_edit_session_date_field"
+		const val DATE_DIALOG = "add_edit_session_date_dialog"
 		const val TOTAL_PAGES_FIELD = "add_edit_session_total_pages"
 		const val START_PAGE_FIELD = "add_edit_session_start_page_field"
 		const val END_PAGE_FIELD = "add_edit_session_end_page_field"
