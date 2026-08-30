42cd08e8677659d05fe37c63b93810833e6231d9
Pedro Guillén <pedro.guillen55@gmail.com>
2026-08-30 13:51:28 +0200

    Implemented testags as resource id to androidapp.

--- androidApp/src/main/kotlin/com/pguillen/readingtracker/MainActivity.kt
+++ androidApp/src/main/kotlin/com/pguillen/readingtracker/MainActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +18,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .semantics {
+                        testTagsAsResourceId = true
+                    }
+            ) {
+                App()
+            }
         }
     }
 }
