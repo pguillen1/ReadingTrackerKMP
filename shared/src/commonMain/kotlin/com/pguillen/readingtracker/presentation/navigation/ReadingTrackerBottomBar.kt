bbc43f7a79dcbc46d610fde46b5980451b0d1597
Pedro Guillén <pedro.guillen55@gmail.com>
2026-08-30 13:50:52 +0200

    Added testags to the bottombar.

--- shared/src/commonMain/kotlin/com/pguillen/readingtracker/presentation/navigation/ReadingTrackerBottomBar.kt
+++ shared/src/commonMain/kotlin/com/pguillen/readingtracker/presentation/navigation/ReadingTrackerBottomBar.kt
@@ -6,6 +6,9 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import com.pguillen.readingtracker.presentation.testtag.ReadingTrackerTestTags
 import com.pguillen.readingtracker.presentation.theme.ReadingTrackerColors
 
 @Composable
@@ -18,8 +21,15 @@ fun ReadingTrackerBottomBar(
 	) {
 		bottomNavDestinations.forEach { destination ->
 			val selected = currentRoute == destination.route
+			val tag = when(destination.label) {
+				"Library" -> ReadingTrackerTestTags.NavigationBar.LIBRARY
+				"Stats" -> ReadingTrackerTestTags.NavigationBar.STATS
+				"Settings" -> ReadingTrackerTestTags.NavigationBar.SETTINGS
+				else -> ""
+			}
 
 			NavigationBarItem(
+				modifier = Modifier.testTag(tag),
 				selected = selected,
 				onClick = {
 					onDestinationClick(destination.route)
