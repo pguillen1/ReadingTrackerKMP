ba43acd864fbcbbc3efd8c7ec003bc5ecb51d602
Pedro Guillén <pedro.guillen55@gmail.com>
2026-09-12 11:05:48 +0200

    Changed koin to initiate in their platforms.

--- shared/src/commonMain/kotlin/com/pguillen/readingtracker/App.kt
+++ shared/src/commonMain/kotlin/com/pguillen/readingtracker/App.kt
@@ -3,26 +3,19 @@ package com.pguillen.readingtracker
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import com.pguillen.readingtracker.di.appModules
 import com.pguillen.readingtracker.presentation.app.AppViewModel
 import com.pguillen.readingtracker.presentation.navigation.AppNavigation
 import com.pguillen.readingtracker.presentation.theme.ReadingTrackerTheme
-import org.koin.compose.KoinApplication
 import org.koin.compose.viewmodel.koinViewModel
-import org.koin.dsl.koinConfiguration
 
 @Composable
 fun App() {
-	KoinApplication(configuration = koinConfiguration {
-		modules(appModules())
-	}) {
-		val appViewModel: AppViewModel = koinViewModel()
-		val uiState by appViewModel.uiState.collectAsState()
+	val appViewModel: AppViewModel = koinViewModel()
+	val uiState by appViewModel.uiState.collectAsState()
 
-		ReadingTrackerTheme(
-			themePreference = uiState.themePreference
-		) {
-			AppNavigation()
-		}
+	ReadingTrackerTheme(
+		themePreference = uiState.themePreference
+	) {
+		AppNavigation()
 	}
 }
--- webApp/src/webMain/kotlin/com/pguillen/readingtracker/main.kt
+++ webApp/src/webMain/kotlin/com/pguillen/readingtracker/main.kt
@@ -2,10 +2,16 @@ package com.pguillen.readingtracker
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import com.pguillen.readingtracker.di.appModules
 import com.pguillen.readingtracker.presentation.components.PhoneDemoFrame
+import org.koin.core.context.startKoin
+import org.koin.mp.KoinPlatform.startKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    startKoin {
+        modules(appModules())
+    }
     ComposeViewport {
         PhoneDemoFrame {
             App()
